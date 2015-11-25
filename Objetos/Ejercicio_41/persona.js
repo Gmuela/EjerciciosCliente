4ec4ba5040a3f42a6f7b446ef1f94ff360db347e
alumno <alumno>
2015-11-25 12:38:33 +0100

    25/11/2015

--- Objetos/Ejercicio_41/persona.js
+++ Objetos/Ejercicio_41/persona.js
@@ -6,5 +6,10 @@ function Persona(matricula, curso, nombre, apellido1, apellido2, fecha){
     this.apellido1=apellido1;
     this.apellido2=apellido2;
     this.fecha=new Date(fecha);
+
+    this.getNombre()=function{
+        
+        return this.nombre;
+    }
     
 }
